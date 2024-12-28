09c3fc9a524286a7886d026c739b23a04d300a03
aswastik11 <anubhavswastik745@gmail.com>
2024-12-28 23:00:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python
 
 import time
-import os
-import json
-import hvac
 from flask import Flask
 app = Flask(__name__)
-# Initialize the Vault client
-client = hvac.Client(url=os.getenv('VAULT_ADDR'))
-
-# Authenticate using the token
-client.token = os.getenv('VAULT_TOKEN')
-
-# Read a secret
-secret = client.secrets.kv.read_secret_version(path='hello-world/config')
-START = time.time()
 
 def elapsed():
     running = time.time() - START
@@ -24,8 +12,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return json.dumps(dict(secret), indent=4)
-    #return "Hello World (Python)! (up %s)\n" % elapsed()
+    return "Hello World (Python)! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
